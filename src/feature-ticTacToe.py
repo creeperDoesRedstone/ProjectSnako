a6c0b65c5c33d3060447236da5506286b032839f
creeperDoesRedstone <142895378+creeperDoesRedstone@users.noreply.github.com>
2023-09-04 15:39:49 +0700

    Update feature-ticTacToe.py

--- src/feature-ticTacToe.py
+++ src/feature-ticTacToe.py
@@ -1,7 +1,13 @@
 import nextcord
-from nextcord import Interaction
+from nextcord import Interaction, ButtonStyle
 from nextcord.ext import commands
 from nextcord.ui import Button, View
 
 class feature-ticTacToe(commands.Cog):
-	def __init__(self, 
+	def __init__(self, client):
+		self.client = client
+
+	@nextcord.slash_command(name='tictactoe',
+				description='Starts the tic tac toe game)
+	async def tictactoe(self, interaction: Interaction):
+		cell1 = Button(label='1', style=ButtonStyle.blurple)
