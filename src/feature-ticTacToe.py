623eaa75680295fb96f48dfe0e87b68fdcf3faae
creeperDoesRedstone <142895378+creeperDoesRedstone@users.noreply.github.com>
2023-09-04 14:00:58 +0700

    feature-ticTacToe.py

---
+++ src/feature-ticTacToe.py
@@ -0,0 +1,7 @@
+import nextcord
+from nextcord import Interaction
+from nextcord.ext import commands
+from nextcord.ui import Button, View
+
+class feature-ticTacToe(commands.Cog):
+	def __init__(self, 
